21837914af744c4c9530db3288e2d917d95f93c2
ign1tion2627 <ign1tion2627@gmail.com>
2026-09-07 15:03:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@
 // used in client-side code. Row Level Security (set up per the
 // SETUP.md instructions) is what keeps the data safe.
 
-const SUPABASE_URL = "https://lsbxfnyqtkttwlgwufvn.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://lsbxfnyqtkttwlgwufvn.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzYnhmbnlxdGt0dHdsZ3d1ZnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1Mjk5NzMsImV4cCI6MjEwMzEwNTk3M30.emKTYrGCTDi3OaSgDCQPPTh--760f3XMIf6xnShg1X8";
